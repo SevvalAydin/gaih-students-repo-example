ba04469e4aea2131d120ef63757c470c261bede1
SevvalAydin <78428327+SevvalAydin@users.noreply.github.com>
2021-02-07 23:59:05 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,12 @@
 #Explain your work
 
 #Question 1
-for x in range(a):
-	print(a)
+username = "SevvalAydin"
+password = "password5"
+
+username1 = input("Please enter your username: ")
+password1 = input("Please enter your password: ")
+
+if (username1 == username1 and password == password1):
+	print("You are logged in")
+
